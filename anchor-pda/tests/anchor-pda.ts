0021339df7a4e92eb657c7f57e9eb72a51cf5b41
4rjunc <arjunmundamani@gmail.com>
2025-01-04 00:04:45 +0530

    imp PDA testing

--- anchor-pda/tests/anchor-pda.ts
+++ anchor-pda/tests/anchor-pda.ts
@@ -2,6 +2,7 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorPda } from "../target/types/anchor_pda";
 import { PublicKey, Keypair } from "@solana/web3.js";
+import { assert } from "chai";
 
 describe("anchor-pda", () => {
   // Configure the client to use the local cluster.
@@ -15,6 +16,8 @@ describe("anchor-pda", () => {
     program.programId
   );
 
+  console.log("PDA:", [PDA]);
+
   it("Is initialized!", async () => {
     const transactionSignature = await program.methods
       .initialize()
@@ -25,10 +28,13 @@ describe("anchor-pda", () => {
       .rpc();
 
     console.log("Transaction Signature:", transactionSignature);
+    assert.equal(0, 0);
   });
 
   it("Fetch Account", async () => {
     const pdaAccount = await program.account.dataAccount.fetch(PDA);
     console.log(JSON.stringify(pdaAccount, null, 2));
+
+    assert.equal(0, 0);
   });
 });
