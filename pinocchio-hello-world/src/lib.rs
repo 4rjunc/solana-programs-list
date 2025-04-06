c6d1a49e3457894b395d462c21882279dbde92b6
4rjunc <arjunmundamani@gmail.com>
2025-04-06 22:12:58 +0530

    Added Lazy program entrypoint

--- pinocchio-hello-world/src/lib.rs
+++ pinocchio-hello-world/src/lib.rs
@@ -1,35 +1,36 @@
-use pinocchio::{account_info::AccountInfo, entrypoint, pubkey::Pubkey, ProgramResult};
+use pinocchio::{
+    default_allocator, default_panic_handler, entrypoint::InstructionContext,
+    lazy_program_entrypoint, ProgramResult,
+};
 
 use pinocchio_log::log;
 
 // Define the program entrypoint
-entrypoint!(process_instruction);
+lazy_program_entrypoint!(process_instruction);
+default_allocator!();
+default_panic_handler!();
 
 // Process instruction entrypoint
 
-pub fn process_instruction(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    instruction_data: &[u8],
-) -> ProgramResult {
+pub fn process_instruction(mut context: InstructionContext) -> ProgramResult {
     // Log a message to the blockchain
     log!("Hello, Solana World!");
 
     // If instruction data is provided, log it as well
-    if !instruction_data.is_empty() {
+    if !context.instruction_data()?.is_empty() {
         // Convert instruction data to a string if possible
-        if let Ok(message) = std::str::from_utf8(instruction_data) {
+        if let Ok(message) = std::str::from_utf8(context.instruction_data()?) {
             log!("Received message: {}", message);
         } else {
-            log!("Received binary data of length: {}", instruction_data.len());
+            log!(
+                "Received binary data of length: {}",
+                context.instruction_data()?.len()
+            );
         }
     }
 
-    // Log the number of accounts provided
-    log!("Number of accounts: {}", accounts.len());
-
     // Log the program ID
-    log!("Program ID: {}", program_id);
+    log!("Program ID: {}", context.program_id()?);
 
     // Return success
     Ok(())
