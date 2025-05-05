fb8fdd0cde6c589d4cf65e39c842adce145635fc
4rjunc <arjunmundamani@gmail.com>
2025-05-05 20:46:36 +0530

    imp: add docs, crate imports to native-token program

--- native-token/src/lib.rs
+++ native-token/src/lib.rs
@@ -1,14 +1,44 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program::invoke,
+    program_pack::Pack,
+    pubkey::Pubkey,
+    rent::Rent,
+    system_instruction,
+    sysvar::Sysvar,
+};
+
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+pub struct TokenArgs {
+    pub title: String,
+    pub symbol: String,
+    pub uri: String,
+    pub decimal: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+entrypoint!(process_instruction);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+fn process_instruction(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let args = TokenArgs::try_from_slice(instruction_data)?;
+
+    let account_iter = &mut accounts.iter();
+
+    let mint_account = next_account_info(account_iter)?;
+    let mint_authority = next_account_info(account_iter)?;
+    let metadata_account = next_account_info(account_iter)?;
+    let payer = next_account_info(account_iter)?;
+    let rent = next_account_info(account_iter)?;
+    let system_program = next_account_info(account_iter)?;
+    let token_program = next_account_info(account_iter)?;
+    let token_metadata_program = next_account_info(account_iter)?;
+
+    Ok(())
 }
