602765d9650f3389e13006a287b1ac806e99a006
4rjunc <arjunmundamani@gmail.com>
2025-05-20 14:58:47 +0530

    imp: added insctruonc

--- native-escrow/src/instruction.rs
+++ native-escrow/src/instruction.rs
@@ -1,5 +1,30 @@
 // instruction.rs defines the "API" of a program
+use crate::error::EscrowError::InvalidInstruction;
+use solana_program::program_error::ProgramError;
 
 pub enum EscrowInstruction {
     InitEscrow { amount: u64 },
+}
+
+impl EscrowInstruction {
+    /// Unpacks a byte buffer into a [EscrowInstruction](enum.EscrowInstruction.html).
+    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
+        let (tag, rest) = input.split_first().ok_or(InvalidInstruction)?;
+
+        Ok(match tag {
+            0 => Self::InitEscrow {
+                amount: Self::unpack_amount(rest)?,
+            },
+            _ => return Err(InvalidInstruction.into()),
+        })
+    }
+
+    fn unpack_amount(input: &[u8]) -> Result<u64, ProgramError> {
+        let amount = input
+            .get(..8)
+            .and_then(|slice| slice.try_into().ok())
+            .map(u64::from_le_bytes)
+            .ok_or(InvalidInstruction)?;
+        Ok(amount)
+    }
 }
