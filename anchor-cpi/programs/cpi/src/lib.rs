f1e3e2b546d83f7fdeaf5f01a3e1267e11585fe1
4rjunc <arjunmundamani@gmail.com>
2025-06-16 00:03:34 +0530

    program: anchor-cpi setting up

--- anchor-cpi/programs/cpi/src/lib.rs
+++ anchor-cpi/programs/cpi/src/lib.rs
@@ -6,11 +6,61 @@ declare_id!("DfrhYpR3Uu751wLkPVC8m6EX4ryQSSzj1XecdcfNEaUj");
 pub mod cpi {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create(ctx: Context<Create>, message: String) -> Result<()> {
+        msg!("Creating message: {:}", message);
+        let account_data = &mut ctx.accounts.message_account;
+        account_data.user = ctx.accounts.user.key();
+        account_data.message = message;
+        account_data.bump = ctx.bumps.message_account;
+        Ok(())
+    }
+
+    pub fn update(ctx: Context<Update>, message: String) -> Result<()> {
+        msg!("Updating message: {}", message);
+        let account_data = &mut ctx.accounts.message_account;
+        account_data.message = message;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(message: String)]
+pub struct Create<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        seeds = [b"message", user.key().as_ref()],
+        bump,
+        payer = user,
+        space = 8 + 32 + 4 + message.len() + 1
+    )]
+    pub message_account: Account<'info, MessageAccount>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(message: String)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"message", user.key().as_ref()],
+        bump = message_account.bump,
+        realloc = 8 + 32 + 4 + message.len() + 1,
+        realloc::payer = user,
+        realloc::zero = true
+    )]
+    pub message_account: Account<'info, MessageAccount>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct MessageAccount {
+    pub user: Pubkey,
+    pub message: String,
+    pub bump: u8,
+}
