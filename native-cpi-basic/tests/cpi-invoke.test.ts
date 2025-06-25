761baeed79e28b1bc33b141d4c1b130fb7c0574c
4rjunc <arjunmundamani@gmail.com>
2025-06-26 00:15:05 +0530

    simple-cpi basic test case done

--- native-cpi-basic/tests/cpi-invoke.test.ts
+++ native-cpi-basic/tests/cpi-invoke.test.ts
@@ -10,18 +10,36 @@ describe('Simple CPI', async () => {
   const program = path.join(`${__dirname}/fixtures`, "cpi_invoke.so")
   svm.addProgramFromFile(programId, program);
 
-  const sender = Keypair.generate();
-  const recipient = Keypair.generate();
+  test('test', () => {
+    const sender = Keypair.generate();
+    const recipient = Keypair.generate();
 
-  const amount = BigInt(LAMPORTS_PER_SOL);
-  svm.airdrop(sender.publicKey, amount);
+    const amount = BigInt(LAMPORTS_PER_SOL);
+    svm.airdrop(sender.publicKey, amount);
 
-  const transferAmount = amount / BigInt(2);
-  const instructionIndex = 0;
+    const transferAmount = amount / BigInt(2);
+    const instructionIndex = 0;
 
-  const data = Buffer.alloc(9);
-  data.writeInt8(instructionIndex, 0);
-  data.writeBigInt64BE(transferAmount, 1);
+    const data = Buffer.alloc(9);
+    data.writeInt8(instructionIndex, 0);
+    data.writeBigInt64BE(transferAmount, 1);
 
-  console.log(data);
+    const ix = new TransactionInstruction({
+      keys: [
+        { pubkey: sender.publicKey, isSigner: true, isWritable: true },
+        { pubkey: recipient.publicKey, isSigner: false, isWritable: true },
+        { pubkey: SystemProgram.programId, isSigner: false, isWritable: false }
+      ],
+      data,
+      programId,
+    })
+
+    const tx = new Transaction().add(ix);
+    tx.recentBlockhash = svm.latestBlockhash();
+    tx.sign(sender);
+
+    //console.log("Transaction: ", tx)
+
+    svm.sendTransaction(tx);
+  })
 })
