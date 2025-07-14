7e6de4125e3f5b47de361f1bc9e3d0050b9c0180
4rjunc <arjunmundamani@gmail.com>
2025-07-14 21:20:07 +0530

    feat: native-escrow add `Escrow` state

--- native-escrow/escrow/src/state/escrow.rs
+++ native-escrow/escrow/src/state/escrow.rs
@@ -11,3 +11,13 @@ pub struct EscrowAccount {
 impl EscrowAccount {
     pub const SEED_PREFIX: &'static str = "escrow";
 }
+
+#[repr(C)]
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+pub struct Escrow {
+    pub seed: u64,
+    pub maker: Pubkey,
+    pub mint_a: Pubkey,
+    pub mint_b: Pubkey,
+    pub receive: u64,
+}
