bd4b256e8c75fd6ac71b2c451f5cc9950ed9f8f4
4rjunc <arjunmundamani@gmail.com>
2025-12-19 12:52:55 +0530

    test: add client code for create mint

--- anchor-spl-token/tests/spl-token.ts
+++ anchor-spl-token/tests/spl-token.ts
@@ -1,16 +1,40 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SplToken } from "../target/types/spl_token";
+import { TOKEN_2022_PROGRAM_ID, getMint } from "@solana/spl-token";
 
 describe("spl-token", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.splToken as Program<SplToken>;
+  const mint = anchor.web3.Keypair.generate();
 
   it("Is initialized!", async () => {
     // Add your test here.
     const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    console.log(`Your transaction signature: https://orbmarkets.io/tx/${tx}?cluster=devnet`);
   });
+
+  it("Create Mint Account", async () => {
+    const tx = await program.methods.createMint()
+      .accounts({
+        mint: mint.publicKey,
+        tokenProgram: TOKEN_2022_PROGRAM_ID
+      })
+      .signers([mint])
+      .rpc();
+    console.log(`Your transaction signature: https://orbmarkets.io/tx/${tx}?cluster=devnet`);
+
+    const mintAccount = await getMint(
+      program.provider.connection,
+      mint.publicKey,
+      "confirmed",
+      TOKEN_2022_PROGRAM_ID
+    )
+
+    console.log("\nMint Account", mintAccount);
+
+  });
+
 });
