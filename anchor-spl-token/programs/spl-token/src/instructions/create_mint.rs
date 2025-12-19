cb98012ca61593b3879e9aa4f82a7629b677140f
4rjunc <arjunmundamani@gmail.com>
2025-12-19 12:36:42 +0530

    feat: add create mint account logic

---
+++ anchor-spl-token/programs/spl-token/src/instructions/create_mint.rs
@@ -0,0 +1,20 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token_interface::{Mint, TokenInterface};
+
+#[derive(Accounts)]
+pub struct CreateMint<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = signer,
+        mint::decimals = 6,
+        mint::authority = signer.key(),
+        mint::freeze_authority = signer.key(),
+    )]
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+}
--- anchor-spl-token/programs/spl-token/src/lib.rs
+++ anchor-spl-token/programs/spl-token/src/lib.rs
@@ -1,13 +1,23 @@
 use anchor_lang::prelude::*;
 
+pub mod instructions;
+
+use instructions::*;
+
 declare_id!("96KJa3QnFirfkBGYqfkpirVbaMWAFYyhPVpq2a1dHmwz");
 
 #[program]
 pub mod spl_token {
+
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+
+    pub fn create_mint(ctx: Context<CreateMint>) -> Result<()> {
+        msg!("creating a mint account: {}", ctx.accounts.mint.key());
         Ok(())
     }
 }
